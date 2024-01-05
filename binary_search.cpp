af8fae8a7e51a75557763da0f6e63d7640cbb731
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-01-06 03:18:38 +0530

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace  std;
+int binary(int arr[],int n,int k)
+{
+  int s=0;
+  int e=n-1;
+  int mid=(s+e)/2;
+  while(s<=e)
+    {
+      if(arr[mid]==k)
+        return k;
+      else if(arr[mid]>k)
+        e=mid-1;
+      else
+        s=mid+1;
+      mid=(s+e)/2;
+    }
+  return -1 ;
+}
+int main()
+{
+  int n,k;
+  cin>>n;
+  int arr[10];
+  cin>>k;
+  for(int i=0;i<n;i++)
+    {
+      cin>>arr[i];
+    }
+  int ans=binary(arr,n,k);
+  if(ans== -1)
+    cout<<"not found";
+  else
+  cout<<"element found "<<ans;
+  return 0;
+}
