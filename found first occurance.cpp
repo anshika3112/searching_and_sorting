3e43a05ecb862e8bfb022f2538b9a0a788598511
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2024-01-06 03:33:02 +0530

    Create found first occurance.cpp

---
+++ found first occurance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace  std;
+int found(int arr[],int n,int k)
+{
+  int s=0;
+  int e=n-1;
+  int mid=(s+(e-s))/2;
+  int ans=-1;
+  while(s<=e)
+    {
+      if(arr[mid]==k)
+      {
+        ans=mid;
+        e=mid-1;
+      }
+      else if(arr[mid]>k)
+        e=mid-1;
+      else
+        s=mid+1;
+      mid=(s+e)/2;
+    }
+  return ans;
+}
+int main()
+{
+  int n,k;
+  cin>>n;
+  int arr[10];
+  cin>>k;
+  for(int i=0;i<n;i++)
+    {
+      cin>>arr[i];
+    }
+  int a=found(arr,n,k);
+  if(a== -1)
+    cout<<"not found";
+  else
+  cout<<"first ocuurance="<<a;
+  return 0;
+}
